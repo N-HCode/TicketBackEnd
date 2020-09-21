1f333389633f296bbc500634de145b84e962fc49
N-HCode <N.HGCHub@gmail.com>
2020-09-20 23:38:26 -0700

    Added swagger documentation. Refactored the organization controller, model, and service.

--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/controller/OrganizationController.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/controller/OrganizationController.java
@@ -2,15 +2,13 @@ package com.github.mhzhou95.javaSpringBootTemplate.controller;
 
 import com.github.mhzhou95.javaSpringBootTemplate.model.Organization;
 import com.github.mhzhou95.javaSpringBootTemplate.service.OrganizationService;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+//Added Swagger documentation. Can be viewed at http://localhost:8080/swagger-ui/#/
 
 @Controller
 @RequestMapping(value = "/organization")
@@ -22,14 +20,9 @@ public class OrganizationController {
         this.service = service;
     }
 
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/findAll")
     public ResponseEntity<?> findAll(){
-
-        Iterable<Organization> allUser = service.findAll();
-        return new ResponseEntity<>(allUser, HttpStatus.OK);
-
+        return service.findAll();
     }
 
     @GetMapping("/{id}")
@@ -39,18 +32,21 @@ public class OrganizationController {
     }
 
     @PostMapping("/create-org")
-    public ResponseEntity<?> createUser(@RequestBody Organization organization){
-        return createUser(organization);
+    public ResponseEntity<?> createOrg(@RequestBody Organization organization){
+        return service.createOrganization(organization);
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+    //Add another response code for the Swagger Documentation
+    @ApiResponse(code = 404, message = "Not Found")
+    public ResponseEntity<?> deleteOrg(@PathVariable Long id){
         //the delete service returns a http response itself.
         return service.delete(id);
     }
 
     @PutMapping("/{id}/edit-name")
-    public ResponseEntity<?> editUser(@PathVariable Long id, @RequestBody String name){
+    public ResponseEntity<?> editOrg(@PathVariable Long id, @RequestBody String name){
         return service.editOrgName(id, name);
     }
 }
--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/controller/TicketController.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/controller/TicketController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/ticket")
 public class TicketController {
     private TicketService service;
 
