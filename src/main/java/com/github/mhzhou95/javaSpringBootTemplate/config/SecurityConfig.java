1357dd0c342020164061b14dc04450e4f9340089
N-HCode <N.HGCHub@gmail.com>
2021-01-17 14:32:23 -0800

    updaing config

--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/SecurityConfig.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.github.mhzhou95.javaSpringBootTemplate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //This allows us to use annotations for authorization on the Controllers
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
