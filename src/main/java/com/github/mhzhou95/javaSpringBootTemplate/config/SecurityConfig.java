cdadccb1d5acf5fb582874fe0f75431501db0899
N-HCode <N.HGCHub@gmail.com>
2021-01-18 19:21:23 -0800

    updated config

--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/SecurityConfig.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.mhzhou95.javaSpringBootTemplate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,4 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true) //This allows us to use annotations for authorization on the Controllers
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       
+    }
 }
