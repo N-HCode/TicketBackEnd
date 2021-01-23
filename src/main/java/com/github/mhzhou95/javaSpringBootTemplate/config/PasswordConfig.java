3ae72f572ada7daf45f25b5ac8a5be9b84eb733e
N-HCode <N.HGCHub@gmail.com>
2021-01-22 16:40:26 -0800

    update config

--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/PasswordConfig.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/config/PasswordConfig.java
@@ -1,7 +1,12 @@
 package com.github.mhzhou95.javaSpringBootTemplate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
+
+    public PasswordEncoder passwordEncoder(){}
+
+
 }
