9e92c9982ae7146dc15b374a70a5a622494ef620
N-HCode <N.HGCHub@gmail.com>
2021-02-15 12:54:24 -0800

    updating Jwt

--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/jwt/JwtConfig.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/jwt/JwtConfig.java
@@ -1,4 +1,27 @@
 package com.github.mhzhou95.javaSpringBootTemplate.jwt;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+//We will add so that it will relate to the resource folder application.properties
+@ConfigurationProperties(prefix = "application.jwt")
 public class JwtConfig {
+
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public JwtConfig() {
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public Integer getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
 }
--- src/main/java/com/github/mhzhou95/javaSpringBootTemplate/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.github.mhzhou95.javaSpringBootTemplate.jwt;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +17,26 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
 
     //All of this is for when the user send the credentials and then we validate those credentials
     private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
 
-    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager
+            , JwtConfig jwtConfig, SecretKey secretKey) {
         this.authenticationManager = authenticationManager;
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
     }
 
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
 }
